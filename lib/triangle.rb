defb253f41b7a0b1e3daeb2f76288a3c6daf1390
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-08-28 13:12:57 +0300

    commit tree 739f747d43ef01060799df5d72ed666b79eae270

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,19 +10,24 @@ class Triangle
   end 
   
   def kind 
-    if (@length1 > 0 && @length2 > 0 && @length3 > 0) && (@length1 + @length3 > @length2 && @length2 + @length3 > @length1 && @length1 + @length3 > @length2)
-      if @length1 == @length2 && @length1 == @length3
-        return :equilateral 
-      elsif @length1 == @length2 || @length2 == @length3 || @length1 == @length3
-        return :isosceles
-      else
-        return :scalene  
-      end   
-    else 
-     raise TriangleError
-    end  
+    if (@length1 <= 0 && @length2 <= 0 && @length3 <= 0) 
+      raise TriangleError
+    end 
+
+    if (@length1 + @length3 < @length2 || @length2 + @length3 < @length1 || @length1 + @length3 < @length2)
+      raise TriangleError
+    end   
+
+    if @length1 == @length2 && @length1 == @length3
+      return :equilateral 
+    elsif @length1 == @length2 || @length2 == @length3 || @length1 == @length3
+      return :isosceles
+    else
+      return :scalene  
+    end   
   end    
 
   class TriangleError < StandardError
   end   
 end
+ 
